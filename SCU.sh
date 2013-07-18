365a9ce25d85b8e14598a91a7f052a9471cfb506
Antony Bailey <support@antonybailey.net>
2013-07-18 11:25:37 +0100

    Init

---
+++ SCU.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.Trash/*
+find /var/catman -type f -atime 7 -print | xargs -- rm -f --
+find /tmp -type f -atime 7 -print | xargs -- rm -f --
+find /var/cache -type f -atime 7 -print | xargs -- rm -f --
+sudo apt-get clean
+clean
